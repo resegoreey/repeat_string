d082006bdf37edecfce19cb96a33f5a225daa989
RESEGO MOTLHASI <remotlhjhb024@student.wethinkcode.co.za>
2025-01-30 10:50:45 +0200

    Added code for repeating a given string with a given number

---
+++ repeat.py
@@ -0,0 +1,9 @@
+#take a string from a user
+user_string = input("Enter a word: ")
+
+#take a number from the user
+user_number = int(input("Enter a number: "))
+
+#multiplying the string with the provided number
+repeat = user_string * user_number
+print(f"Here is your repeated word: {repeat}")
